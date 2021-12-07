22be254067677adf75781715a3c72acdeed72990
Rachel Powers <rae.powers621@gmail.com>
2021-12-07 14:57:52 -0500

    updated saved tab to display user saved artists

--- lib/database.dart
+++ lib/database.dart
@@ -8,29 +8,52 @@ import 'package:mobile_final/spotify.dart';
 class Database {
   final FirebaseFirestore _db = FirebaseFirestore.instance;
   final uid = Authentication().getUserId();
-  var artistname;
+  var artistnames =[];
+  var names = [];
+  var docs;
+
 
   getArtists() async {
     print(uid);
+
     try {
       final QuerySnapshot result = await FirebaseFirestore.instance
           .collection('artists')
           .where('user', isEqualTo: uid)
           .get();
-      List lResult = result.docs.toList();
-
-      var docs = result.docs.map((e) => e.data());
-      // print(docs['artistID']);
 
 
+      docs = result.docs.map((e) => e.data());
 
-      if (result.size <= 0) {
-        return null;
-      }else{
-        return result as List;
-      }
-    }catch(e){}
 
+
+       if (result.size >= 0) {
+         try {
+           for (var artistName in docs) {
+             this.artistnames.add(artistName);
+           }
+           for (var i = 0; i < artistnames.length; i++) {
+             names.add(artistnames[i]['artistName']);
+           }
+
+           //       for (var k in artistnames) {
+           //         for (var v in k) {
+           //           if (v == 'artistName') {
+           //             this.names.add(artistnames[v]);
+           //           }
+           //         }
+           //       }
+           //
+         } catch (e) {}
+       }
+      //
+      //   print(names);
+      //   return names;
+      // }else{
+      //   return null;
+      // }
+     }catch(e){}
+    return names;
   }
 
   Future<String> addArtist(id,name) async{
