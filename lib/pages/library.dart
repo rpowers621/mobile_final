a5247f190ae32cc11c17915e33934f1ccf379789
famedahossain <60082469+famedahossain@users.noreply.github.com>
2021-12-07 23:36:53 -0500

    Saved Page Created and displays artist top tracks upon press

--- lib/pages/library.dart
+++ lib/pages/library.dart
@@ -1,11 +1,13 @@
+import 'dart:ui';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_final/authentication.dart';
+import 'package:spotify/spotify.dart';
 
 import '../database.dart';
 import '../spotify.dart';
-
 
 
 class LibraryPage extends StatefulWidget {
@@ -15,22 +17,20 @@ class LibraryPage extends StatefulWidget {
   _LibraryPageState createState() => _LibraryPageState();
 }
 
-
 class _LibraryPageState extends State<LibraryPage> {
 
   String id =Authentication().getUserId();
   final FirebaseFirestore fb = FirebaseFirestore.instance;
-  User? user = FirebaseAuth.instance.currentUser;
-  var tracks;
-
+  // var tracks;
+  var hold;
 
   @override
   Widget build(BuildContext context){
-
     return Scaffold(
-
       backgroundColor: Colors.indigo,
       body: Column(children: <Widget>[
+        Text("Click an artist to see their top tracks!", style: const TextStyle(fontSize: 30, color: Colors.white), textAlign: TextAlign.center),
+        SizedBox(width: 30, height: 10,),
         FutureBuilder(
           future:  Database().getArtists(),
           builder: (context,AsyncSnapshot snapshot){
@@ -53,6 +53,7 @@ class _LibraryPageState extends State<LibraryPage> {
                               .showSnackBar(SnackBar(content: Text('$name removed')));
                         },
                         background: Container(color: Colors.red),
+
                         child:
                         ElevatedButton(
                             style: OutlinedButton.styleFrom(
@@ -64,12 +65,9 @@ class _LibraryPageState extends State<LibraryPage> {
                                 ),
                                 fixedSize: Size.fromWidth(500)
                             ),
-
                             onPressed: () async {
-                              tracks =  await Spotify().getTopTracks(id);
-
+                              hold = await Spotify().getArtistId(name) as String;
                               setState(() {
-
                               });
                             },
                             child:  Text(name,
@@ -86,6 +84,35 @@ class _LibraryPageState extends State<LibraryPage> {
             }
           },
         ),
+        SizedBox(width: 50, height: 20,),
+        FutureBuilder(
+            future: Spotify().getTopTracks(hold),
+            builder: (context, AsyncSnapshot snapshot){
+              if(snapshot.hasData){
+                final data = snapshot.data;
+                return ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemCount: data!.length,
+                  itemBuilder: (context, index){
+                    var tracks = data[index];
+                    return
+                      Expanded(
+                        child:
+                        Text(
+                          "${index+1}. $tracks",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 20, color: Colors.white),
+                        ),
+                      );
+                  }
+                );
+              }else{
+                return Container(
+                  child: Text(""),
+                );
+              }
+            }),
       ]
       ),
       floatingActionButton: FloatingActionButton(
@@ -98,4 +125,7 @@ class _LibraryPageState extends State<LibraryPage> {
     );
   }
 }
+
+
+
 
